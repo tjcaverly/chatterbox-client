1632f23efa317a409bd01f4d67030bfb29e082f8
MKS-Student <mks.student@hackreactor.com>
2015-03-16 16:24:33 -0700

    Complete display messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,37 @@
 // YOUR CODE HERE:
 
+var retrieveMessages = function(){
+	$.ajax({
+  	// This is the url you should use to communicate with the parse API server.
+	  url: 'https://api.parse.com/1/classes/chatterbox',
+	  type: 'GET',
+	  data: 'order=-createdAt',
+	  contentType: 'application/json',
+	  success: function (data) {
+	    console.log('chatterbox: Message retrieved');
+	    console.log(data);
+
+	    displayMessage(data);
+	  },
+	  error: function (data) {
+	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	    console.error('chatterbox: Failed to retrieve messages');
+
+	  }
+	});
+};
+
+var addMessage = function(message) {};
+
+var updateMessages = function() {}
+var displayMessage = function(data) {
+
+	for (var i=0; i<data.results.length; i++){
+		var message = data.results[i];
+		$('.content').append("<p>" + "<span class='username'>" + message.username + 
+			"</span>: <span class='text'>" + message.text+ "</span> <span class='time'>" + message.createdAt+"</span></p>");
+	}
+}
+setInterval(updateMessages, 2000);
+
+retrieveMessages();
