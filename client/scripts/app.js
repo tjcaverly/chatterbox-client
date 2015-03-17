74e25826b0e10851a2640867ed008a6fa3ef550f
MKS-Student <mks.student@hackreactor.com>
2015-03-17 11:29:11 -0700

    Add friends working.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,7 @@ var app = {};
 // This is the url you should use to communicate with the parse API server.
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 app.roomnames = {};
+app.friends = {};
 //Setting current room
 app.currentRoomName = "lobby";
 
@@ -16,6 +17,7 @@ app.fetch = function(roomname){
 	  success: function (data) {
 	    console.log('chatterbox: Message retrieved');
 	    displayMessage(data);
+	    console.log(data);
 	  },
 	  error: function (data) {
 	    console.error('chatterbox: Failed to retrieve messages');
@@ -23,13 +25,19 @@ app.fetch = function(roomname){
 	});
 };
 
+app.handleSubmit = function(){
+	console.log('submit');
+};
+
 //submit button for message
-$("#submit").click (function(){
+$("#send .submit").on('click', function(){
+
+	app.handleSubmit();
 	var get = function (name){
    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
       return decodeURIComponent(name[1]);
 	}
-	var text = $("#textField").val();
+	var text = $("#message").val();
 	var message = {
 		username: get("username"),
 		text: text,
@@ -66,10 +74,17 @@ var displayMessage = function(data) {
 		app.addMessage(data.results[i]);
 	}
 
+
+
 	app.clearRooms();
 	for (var i in app.roomnames) {
 		app.addRoom(escapeHtml(i));
 	}
+}
+
+app.addFriend = function(username) {
+	console.log(username);
+	app.friends[escapeHtml(username)] = true;
 }
 
 app.clearMessages = function(){
@@ -78,7 +93,7 @@ app.clearMessages = function(){
 
 app.clearRooms = function(){
 	$('#roomSelect').empty();
-	//app.roomnames = {};
+
 }
 
 //escaping 
@@ -97,12 +112,29 @@ var entityMap = {
 		  });
 		}
 
+
+
 app.addMessage = function(message) {
 	app.roomnames[message.roomname] = true;
+
 	if (message.roomname === app.currentRoomName) {
-	  $('#chats').append("<p>" + "<span class='username'>" + escapeHtml(message.username) + 
-		  "</span>: <span class='text'>" + escapeHtml(message.text) + "</span> <span class='time'>" + 
-			  escapeHtml(message.createdAt)+"</span></p>");
+
+
+		if (escapeHtml(message.username) in app.friends) {
+			var textNode = "<span class='text friend'>" + escapeHtml(message.text) + "</span>";
+	  } else {
+	  	var textNode = "<span class='text'>" + escapeHtml(message.text) + "</span>";
+	  }
+	  var userNameNode = "<span class='username'>" + escapeHtml(message.username) + "</span>";
+
+
+	  var node = "<p>" + userNameNode + ": " + textNode +  " <span class='time'>" + 
+			  escapeHtml(message.createdAt)+"</span></p>";
+
+	  $('#chats').append(node).find('.username').on('click', function(){ 
+	  	app.addFriend($(this).text());
+	  });
+
 	}
 }
 
