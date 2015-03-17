9695644bbbf506ab3c60a4ed53759dfa78bc539b
MKS-Student <mks.student@hackreactor.com>
2015-03-16 17:20:33 -0700

    User can post messages, escape javascript injection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,6 +8,7 @@ var retrieveMessages = function(){
 	  data: 'order=-createdAt',
 	  contentType: 'application/json',
 	  success: function (data) {
+	  	//debugger;
 	    console.log('chatterbox: Message retrieved');
 	    console.log(data);
 
@@ -21,17 +22,77 @@ var retrieveMessages = function(){
 	});
 };
 
-var addMessage = function(message) {};
+//submit button for message
+$("button").click (function(){
+	var text = $("#textField").val();
+	addMessage(text);
+	$('#textField').val(''); //empty textfield
+});
 
-var updateMessages = function() {}
+
+
+//adding message by current user
+var addMessage = function(text) {
+	//get username
+	var get = function (name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+	}
+
+	var message = {
+	  username: get("username"),
+	  text: text,
+	  roomname: '4chan'
+	};
+
+
+	$.ajax({
+	  // This is the url you should use to communicate with the parse API server.
+	  url: 'https://api.parse.com/1/classes/chatterbox',
+	  type: 'POST',
+	  data: JSON.stringify(message),
+	  contentType: 'application/json',
+	  success: function (data) {
+	    console.log('chatterbox: Message sent');
+	  },
+	  error: function (data) {
+	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	    console.error('chatterbox: Failed to send message');
+	  }
+	});
+};
+
+
+
 var displayMessage = function(data) {
+	var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+	var escapeHtml = function(string) {
+	  return String(string).replace(/[&<>"'\/]/g, function (s) {
+	    return entityMap[s];
+	  });
+	}
+	$('.content').empty();
 
 	for (var i=0; i<data.results.length; i++){
 		var message = data.results[i];
-		$('.content').append("<p>" + "<span class='username'>" + message.username + 
-			"</span>: <span class='text'>" + message.text+ "</span> <span class='time'>" + message.createdAt+"</span></p>");
+		$('.content').append("<p>" + "<span class='username'>" + escapeHtml(message.username) + 
+			"</span>: <span class='text'>" + escapeHtml(message.text) + "</span> <span class='time'>" + escapeHtml(message.createdAt)+"</span></p>");
 	}
 }
-setInterval(updateMessages, 2000);
+
+
+setInterval(retrieveMessages, 1000);
 
 retrieveMessages();
+
+
+
+
